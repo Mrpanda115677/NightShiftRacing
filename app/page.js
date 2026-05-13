c6d4208bf1198c870c4ccc56c6a7c90394b89fad
Anthony Kelley <mrpanda11567@gmail.com>
2026-05-13 14:59:16 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>My Merch Store</h1>
+      <p>Built with Next.js + Vercel + Printify</p>
+    </main>
+  );
+}
